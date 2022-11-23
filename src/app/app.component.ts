cf903cce6c8d5963683635579ca629248da2e91f
Mateus <mateusrocha.pereira2@gmail.com>
2022-11-23 17:52:10 -0300

    Configuração de Header e footer -Foi configurado o Header e footer da página inicial nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hotel-pet-angular';
+  title = 'premier-front';
 }
